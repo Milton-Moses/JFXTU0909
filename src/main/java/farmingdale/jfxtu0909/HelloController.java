9ee5293e45fac36eab8c748d59d2f116466bfced
Milton-Moses <miltonmoses05@gmail.com>
2025-09-09 18:54:27 -0400

    Changed the text from pressing the button

--- src/main/java/farmingdale/jfxtu0909/HelloController.java
+++ src/main/java/farmingdale/jfxtu0909/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to JavaFX Application! This class is CSC325 Software engineering");
     }
 }
